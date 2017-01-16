729c3c3ba6f0a1f81f02fdbd5e359c13571ea8ee
Trevor <cobaltrisen@gmail.com>
2017-01-16 11:32:39 -0500

    first commit

---
+++ Assets/Menu/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MenuManager : MonoBehaviour {
+    public GameObject[] frames = { };
+    public string currentFrame;
+    void Start() {
+        currentFrame = "Main";
+    }
+    public void GoToFrame(string frame) {
+        GetComponent<AudioSource>().Play();
+        GameObject.Find(currentFrame).GetComponent<Animator>().SetTrigger("out");
+        foreach (GameObject g2 in frames) {
+            if (g2.name == frame) {
+                g2.GetComponent<Animator>().SetTrigger("in");
+                currentFrame = g2.name;
+            }
+        }
+    }
+    public void Exit() {
+        Application.Quit();
+    }
+}
---
+++ Assets/PlayVideo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayVideo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MovieTexture mt = GetComponent<RawImage>().texture as MovieTexture;
+        mt.loop = true;
+        mt.Play();
+        GetComponent<AudioSource>().Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

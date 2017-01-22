b97f47ab042f256e5c685310a5695453bfd9c8a6
CobaltRisen <cobaltrisen@gmail.com>
2017-01-22 09:06:03 -0500

    Server browser stuff and Unet

---
+++ Assets/Scripts/Matchmaker.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Matchmaker : MonoBehaviour {
+    NetworkManager nManager;
+    NetworkMatch mm;
+    public GameObject matchTemplate;
+    void Start() {
+        nManager = GetComponent<NetworkManager>();
+        nManager.StartMatchMaker();
+        mm = nManager.matchMaker;
+        FindInternetMatches();
+    }
+    public void CreateInternetMatch(String name) {
+        mm.CreateMatch(name, (uint)4, true, "", "", "", 0, 0, nManager.OnMatchCreate);
+    }
+
+    public void FindInternetMatches() {
+        foreach (Transform g in GameObject.Find("ServerList").transform) {
+            if (g.gameObject.activeInHierarchy == true) {
+                Destroy(g.gameObject);
+            }
+        }
+        mm.ListMatches(0, 10, "", true, 0, 0, OnMatchList);
+    }
+
+    void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matches) {
+        Debug.Log("list");
+        print(matches.Count+" matches found");
+        foreach (MatchInfoSnapshot mis in matches) {
+            GameObject template = (GameObject)Instantiate(matchTemplate);
+            template.GetComponent<ServerInfo>().match = mis;
+            template.transform.SetParent(GameObject.Find("ServerList").transform, false);
+            template.name = mis.name;
+            template.SetActive(true);
+        }
+    }
+    public void JoinMatch (MatchInfoSnapshot mis){
+            mm.JoinMatch(mis.networkId, "", "", "", 0, 0, nManager.OnMatchJoined);
+    }
+}
---
+++ Assets/Scripts/ServerInfo.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Networking.Match;
+public class ServerInfo : MonoBehaviour {
+    public MatchInfoSnapshot match;
+    public string name;
+    public int players;
+    public int playerCap;
+    bool joined;
+	// Use this for initialization
+	void Start () {
+        joined = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (match != null) {
+            name = match.name;
+            players = match.currentSize;
+            playerCap = match.maxSize;
+            transform.Find("Name").GetComponent<Text>().text = name;
+            transform.Find("Players").GetComponent<Text>().text = players+"/"+playerCap+"    ";
+            GetComponent<Button>().onClick.AddListener(Join);
+        }
+	}
+    void Join() {
+        if (joined == false) {
+            GameObject.Find("NetworkManager").GetComponent<Matchmaker>().JoinMatch(match);
+            joined = true;
+        }
+    }
+}
